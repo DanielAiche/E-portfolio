0109ef41c8f39dfbf5953b86e843090e257a49c4
Daniel Aiche <daniel.aiche@icloud.com>
2024-02-29 21:17:10 +1100

    Made some changes

---
+++ E-Portfolio/src/app/components/AboutSection.jsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python</li>
+        <li>Java</li>
+        <li>HTML</li>
+        <li>CSS (Tailwind)</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>R</li>
+        <li>Node.js</li>
+        <li>Git</li>
+        <li>Go</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Frontend Simplified</li>
+        <li>University of Sydney - Computer Science and Finance </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full-stack software engineer with a passion for creating
+            great user experiences. I have experience
+            working with Python, Java, JavaScript, R, GO, React, Tailwind CSS, Node.js, HTML, CSS, and Git. I am an eager and fast learner and am always
+            looking to expand my knowledge and skillset.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              My Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ E-Portfolio/src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#797e9700] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p className="text-slate-100">Copyright © Daniel Aiche 2024.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
